9f652242cae5633098326056bdd69a5c31638071
lianglee <manactric@gmail.com>
2013-01-19 06:34:43 -0800

    view ajax box

---
+++ views/default/forms/blockuser/ajax/view.php
@@ -0,0 +1,7 @@
+		<div id="wait">
+<h2> Block a user</h2>		
+<p>If you find thid user fake, spam then report to Administrator.</p>
+<p> Are you sure to block this user? </p>
+	<?php echo elgg_view('input/submit', array('value' => elgg_echo('block'))); ?>
+
+</div>
